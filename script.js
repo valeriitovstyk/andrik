849d6d873f5971a50f8e9653f6a0538831407e2c
valeriitovstyk <valeriitovstyk@gmail.com>
2019-06-06 14:47:55 +0300

    test

---
+++ script.js
@@ -0,0 +1,16 @@
+const login = document.querySelector('.login');
+
+function validateInput(e) {
+    const input = e.target.value;
+    const isName = /[a-z]+/.exec(input);
+    const isDigit = /\d+/.exec(input);
+    if (isName) {
+        console.log({"login": input})
+        return {"login": input};
+    }
+    return isDigit
+        ? console.log({"email": input})
+        : console.log("say user fuck");
+
+}
+login.addEventListener("blur", validateInput);
